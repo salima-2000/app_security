4e4fa6e332cc63bbac9ea1a7f1b8e67eed8877b8
salima-2000 <zariouh2000salima@gmail.com>
2022-05-23 14:30:27 +0200

    attaque fileUpload

--- src/main/java/com/aseds/inpt/appsecurity/controller/UploadImageController.java
+++ src/main/java/com/aseds/inpt/appsecurity/controller/UploadImageController.java
@@ -5,31 +5,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-
-
-
 @Controller
-public class ProduitController {
+public class UploadImageController {
 	
-	
-	@GetMapping("/produits/images")
+	@GetMapping("/images")
 	public String afficherImagesProduits() {
 		return "pages/uploadImage";
 	}
-	@PostMapping("/produits/upload")
+	@PostMapping("/images/upload")
 	public String uploaderImages(@RequestParam("file") MultipartFile file,HttpSession httpSession) throws IOException {
 	    String folder ="C:\\Users\\Perfect PC\\Downloads\\appSecurity\\appSecurity\\src\\main\\resources\\uploads\\";
 	    Path path=Paths.get(folder+ file.getOriginalFilename());
 	    byte[] bytes = file.getBytes();
-	    Files.write(path, bytes);
-	    httpSession.setAttribute("etat", "succes");	    
-		return "redirect:/produits/images";
+	    try {
+	    	 Files.write(path, bytes);
+	 	    httpSession.setAttribute("etat", "succes");	
+	    }catch (Exception e) {
+	    	
+	    	httpSession.setAttribute("etat", "erreur");	
+	    }
+	       
+		return "redirect:/images";
 	}
 }
